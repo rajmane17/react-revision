23f9e2f22b819a8a08645ec0e1a8042ab179567e
Raj Mane <rajmane9594@gmail.com>
2025-05-25 23:06:09 +0530

    started tailwind

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,35 @@
+import StartPage from "./pages/StartPage"
+
+
+function App() {
+
+  return (
+    // <div className='flex justify-around'>
+    //   <div className='bg-blue-500'>This is div 1</div>
+    //   <div className='bg-red-500'>This is div 2</div>
+    //   <div className='bg-gray-500'>This is div 3</div>
+    // </div>
+
+    // <div className="grid grid-cols-10">
+    //   <div className="col-span-3 bg-amber-200">This is div 1</div>
+    //   <div className="col-span-3 bg-red-400">This is div 2</div>
+    //   <div className="col-span-4 bg-pink-600">This is div 3</div>
+    // </div>
+
+    // <div className="sm:bg-blue-400 bg-red-500">
+    //   <p>This means that when the size is small or above small, blue will
+    //     appear as bg color.
+    //   </p>
+    // </div>
+
+    // <div className="sm:grid grid-cols-10">
+    //   <div className="sm:col-span-3 bg-amber-200">This is div 1</div>
+    //   <div className="sm:col-span-3 bg-red-400">This is div 2</div>
+    //   <div className="sm:col-span-4 bg-pink-600">This is div 3</div>
+    // </div>
+    
+    <StartPage />
+  )
+}
+
+export default App
---
+++ tailwind/src/pages/StartPage.jsx
@@ -0,0 +1,11 @@
+
+
+const StartPage = () => {
+    return (
+        <div className="h-screen w-screen bg-[#04244c] text-white">
+            Hii
+        </div>
+    )
+}
+
+export default StartPage
